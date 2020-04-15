a0c3e3fd0eb9d0b9429e06c3a91a6ea3b722d40c
Sixto Ortega <sixto_miguel@hotmail.com>
2020-04-15 12:46:09 -0400

    Adding tasks controller

---
+++ TasksApp/TasksApp.ApiSqlServer/Controllers/TasksController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TasksApp.ApiSqlServer.Data;
+using TasksApp.ApiSqlServer.Models;
+
+namespace TasksApp.ApiSqlServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Task>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Models.Task>> GetTask(string id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+            return task;
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(string id, Models.Task task)
+        {
+            if (id != task.Id) return BadRequest();
+            _context.Entry(task).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public async Task<ActionResult<Models.Task>> PostTask(Models.Task task)
+        {
+            _context.Tasks.Add(task);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TaskExists(task.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTask", new { id = task.Id }, task);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Models.Task>> DeleteTask(string id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return task;
+        }
+
+        private bool TaskExists(string id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
--- TasksApp/TasksApp.ApiSqlServer/Startup.cs
+++ TasksApp/TasksApp.ApiSqlServer/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TasksApp.ApiSqlServer.Data;
 
 namespace TasksApp.ApiSqlServer
 {
@@ -25,6 +27,12 @@ namespace TasksApp.ApiSqlServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddControllers().AddNewtonsoftJson(options =>
+            //    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            //);
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllers();
         }
 
